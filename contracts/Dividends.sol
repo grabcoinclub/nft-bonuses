d4e611b7a587fbe21bdb11605f08ced64efb2a68
Victor Han <86858790+sunmoon11100@users.noreply.github.com>
2022-10-12 13:24:15 +0300

    Updated dividends

--- contracts/Dividends.sol
+++ contracts/Dividends.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -17,6 +18,10 @@ interface IGrabCoinClub {
 }
 
 contract Dividends is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20Metadata;
+
+    uint8 constant MULTIPLIER = 10;
+
     IGrabCoinClub private _nftToken;
     IERC20Metadata private _token;
 
@@ -25,34 +30,40 @@ contract Dividends is Ownable, ReentrancyGuard {
     uint256 public constant seriesTotal = 15;
     uint256[seriesTotal] public dividends;
 
+    event UpdatedNFTToken(address indexed _address);
+    event UpdatedToken(address indexed _address);
+    event GrabbedDividends(address indexed _reciever, uint256 _value);
+
     constructor(address nftToken, address token) {
         _nftToken = IGrabCoinClub(nftToken);
         _token = IERC20Metadata(token);
         dividends = [
-            47,
-            75,
-            131,
-            244,
-            469,
-            750,
-            1313,
-            2438,
-            4688,
-            7500,
-            13125,
-            24375,
-            46875,
-            75000,
-            131250
+            47, // 4.7
+            75, // 7.5
+            131, // 13.1
+            244, // 24.4
+            469, // 46.9
+            750, // 75
+            1313, // 131.3
+            2438, // 243.8
+            4688, // 468.8
+            7500, // 750
+            13125, // 1312.5
+            24375, // 2437.5
+            46875, // 4687.5
+            75000, // 7500
+            131250 // 13125
         ];
     }
 
     function setNFTTokenAddress(address _address) external onlyOwner {
         _nftToken = IGrabCoinClub(_address);
+        emit UpdatedNFTToken(_address);
     }
 
     function setTokenAddress(address _address) external onlyOwner {
         _token = IERC20Metadata(_address);
+        emit UpdatedToken(_address);
     }
 
     function calcDividend(uint256 tokenId) public view returns (uint256) {
@@ -61,19 +72,27 @@ contract Dividends is Ownable, ReentrancyGuard {
             : _lastGrabAt[tokenId];
 
         return
-            dividends[_nftToken.seriesOf(tokenId)] *
-            _diffMonths(timestamp1, block.timestamp);
+            (dividends[_nftToken.seriesOf(tokenId)] *
+                _diffMonths(timestamp1, block.timestamp) *
+                (10**_token.decimals())) / MULTIPLIER;
     }
 
     function grabDividends() external nonReentrant returns (uint256) {
-        uint256 total;
+        uint256 total = 0;
         uint256[] memory tokens = _nftToken.tokensOfOwner(msg.sender);
         for (uint256 i = 0; i < tokens.length; i++) {
             total += calcDividend(tokens[i]);
             _lastGrabAt[tokens[i]] = block.timestamp;
         }
 
-        _token.transfer(msg.sender, (total * 10**_token.decimals()) / 10);
+        require(
+            total <= _token.balanceOf(address(this)),
+            "Insufficient balance"
+        );
+
+        _token.safeTransfer(msg.sender, total);
+        emit GrabbedDividends(msg.sender, total);
+
         return total;
     }
 
